5bd06d4083064ddc1e18146988ce2ddbb4d19b40
mattb <mkbeneventi@hotmail.com>
2019-01-28 20:44:09 -0500

    add

---
+++ assets/js/app.js
@@ -0,0 +1,99 @@
+// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 75,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("/assets/data/data.csv")
+  .then(function(healthData) {
+       
+    //  Parse Data/Cast as numbers
+    
+    healthData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+      });
+
+    // Create scale functions
+    
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(healthData, d=>d.poverty) * 0.8,
+        d3.max(healthData, d => d.poverty)* 1.2])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(healthData, d => d.healthcare)])
+      .range([height, 0]);   
+
+      // Create axis functions
+    
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+        // Append Axes to the chart
+    
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+// Create Circles
+      var circlesGroup = chartGroup.selectAll("circle")
+      .data(healthData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", "10")
+      .attr("fill", "blue")
+      .attr("opacity", ".5");
+
+      // text in circles using state abbr
+      chartGroup.selectAll("text.text")
+      .data(healthData)
+      .enter()
+      .append("text")
+      .classed("text",true)
+      .text(d => d.abbr)
+      .attr("x", d => xLinearScale(d.poverty))
+      .attr("y", d => yLinearScale(d.healthcare))
+      .attr("dy",5)
+      .attr("text-anchor","middle")
+      .attr("font-size","10px");
+      
+
+// Create axes labels
+      chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .classed("health-text text", true)
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("dy", "1em")
+      .classed("poverty-text text", true)
+      .text("In Poverty (%)");
+  });
